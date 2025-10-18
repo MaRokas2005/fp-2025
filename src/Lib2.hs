102735104b966a7d9b9a980e287484c734f2d46c
Rokas Braidokas <rokasbraidokas@gmail.com>
2025-10-18 10:42:03 +0000

    Added pmap and and3, and4, and5

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -12,55 +12,74 @@ import Data.List (isPrefixOf)
 type ErrorMsg = String
 type Parser a = String -> Either ErrorMsg (a, String)
 
--- Combinators
+pmap :: (a -> b) -> Parser a -> Parser b
+pmap f p input = case p input of
+      Left err -> Left err
+      Right (v, rest) -> Right (f v, rest)
+
 orElse :: Parser a -> Parser a -> Parser a
-orElse p q s = case p s of
+orElse p1 p2 input = case p1 input of
   Right r -> Right r
-  Left _  -> q s
+  Left _  -> p2 input
 
 and2 :: Parser a -> Parser b -> Parser (a,b)
-and2 p q s = case p s of
+and2 p1 p2 input = case p1 input of
   Left e -> Left e
-  Right (a,s1) -> case q s1 of
+  Right (a, rest1) -> case p2 rest1 of
     Left e -> Left e
-    Right (b,s2) -> Right ((a,b), s2)
+    Right (b, rest2) -> Right ((a, b), rest2)
+
+and3 :: Parser a -> Parser b -> Parser c -> Parser (a, b, c)
+and3 p1 p2 p3 input = case and2 p1 p2 input of
+    Left e -> Left e
+    Right ((v1, v2), rest1) -> case p3 rest1 of
+        Left e -> Left e
+        Right (v3, rest2) -> Right ((v1, v2, v3), rest2)
+
+and4 :: Parser a -> Parser b -> Parser c -> Parser d -> Parser (a, b, c, d)
+and4 p1 p2 p3 p4 input = case and3 p1 p2 p3 input of
+    Left e -> Left e
+    Right ((v1, v2, v3), rest1) -> case p4 rest1 of
+        Left e -> Left e
+        Right (v4, rest2) -> Right ((v1, v2, v3, v4), rest2)
+
+and5 :: Parser a -> Parser b -> Parser c -> Parser d -> Parser e -> Parser (a, b, c, d, e)
+and5 p1 p2 p3 p4 p5 input = case and4 p1 p2 p3 p4 input of
+    Left e -> Left e
+    Right ((v1, v2, v3, v4), rest1) -> case p5 rest1 of
+        Left e -> Left e
+        Right (v5, rest2) -> Right ((v1, v2, v3, v4, v5), rest2)
 
 -- >>> parseLiteral "Dump " "Dump Examplesrest"
 -- Right ("Dump ","Examplesrest")
 parseLiteral :: String -> Parser String
-parseLiteral pref s =
-  if pref `isPrefixOf` s
-  then Right (pref, drop (length pref) s)
-  else Left ("Expected " ++ show pref)
+parseLiteral pref = pmap (const pref) match
+  where
+    match :: Parser String
+    match s
+      | pref `isPrefixOf` s = Right (pref, drop (length pref) s)
+      | otherwise = Left ("Expected " ++ show pref)
 
 parseChar :: Char -> Parser Char
-parseChar c s = case s of
-  (x:xs) | x == c -> Right (x, xs)
-  _ -> Left ("Expected char " ++ show c)
+parseChar c = pmap (const c) (parseLiteral [c])
 
--- >>> parseDishName "\"Chicken Soup\"restbv h\""
--- Right ("Chicken Soup","restbv h\"")
-parseString :: Parser String
-parseString s =
-  let (content, rest) = span (/= '"') s
-      valid = case content of
-        [] -> False
-        _  -> isValid content
-  in case rest of
-      ('"':xs) | valid -> Right (content, xs)
-      _ -> Left "Unterminated or invalid string"
-  where
-    isValid :: String -> Bool
-    isValid xs =
-      let parts = words xs
-      in not (null parts)
-         && all (all isAlpha) parts
-         && xs == unwords parts
-
+-- >>> parseDishName "\"Chicken Soupbh\" restbv h"
+-- Right ("Chicken Soupbh"," restbv h")
 parseDishName :: Parser String
-parseDishName s = case parseChar '"' s of
-  Left e -> Left e
-  Right (_, s1) -> parseString s1
+parseDishName = pmap (\(_, name, _) -> name) (and3 (parseChar '"') inner (parseChar '"'))
+  where
+    inner :: Parser String
+    inner s =
+      let (content, rest) = span (/= '"') s
+          valid = case content of
+                    [] -> False
+                    _  -> let parts = words content
+                          in not (null parts)
+                             && all (all isAlpha) parts
+                             && content == unwords parts
+      in case rest of
+           ('"':_) | valid -> Right (content, rest)
+           _               -> Left "Unterminated or invalid string"
 
 -- >>> parseNonZeroDigit "8954 \"Chicken Soup\"restbv"
 -- Right ('8',"954 \"Chicken Soup\"restbv")
@@ -69,27 +88,28 @@ parseNonZeroDigit s = case s of
   (c:cs) | c >= '1' && c <= '9' -> Right (c, cs)
   _ -> Left "Expected non-zero digit"
 
+
+-- >>> parseDigits "8954 \"Chicken Soup\"restbv"
+-- >>> parseDigits "abc"
+-- Right ("8954"," \"Chicken Soup\"restbv")
+-- Right ("","abc")
+parseDigits :: Parser String
+parseDigits s = let (ds, rest) = span isDigit s in Right (ds, rest)
+
 -- >>> parseInteger "800954 \"Chicken Soup\"restbv"
 -- Right (800954," \"Chicken Soup\"restbv")
 -- >>> parseInteger "5597fd"
 -- Right (5597,"fd")
 parseInteger :: Parser Integer
 parseInteger s = case s of
-  ('0':_) ->
-    Left "Leading zeros not allowed"
-  _ -> case parseNonZeroDigit s of
-        Left e -> Left e
-        Right (d, s1) ->
-          let (ds, s2) = span isDigit s1
-          in Right (read (d:ds), s2)
+  ('0':_) -> Left "Leading zeros not allowed"
+  _ -> pmap (\(d, ds) -> read (d:ds))
+            (and2 parseNonZeroDigit parseDigits) s
 
 -- >>> parseDumpable "Examplesrestbv"
 -- Right (Examples,"restbv")
 parseDumpable :: Parser Lib1.Dumpable
-parseDumpable s =
-  case parseLiteral "Examples" s of
-    Right (_, rest) -> Right (Lib1.Examples, rest)
-    Left e -> Left e
+parseDumpable = pmap (const Lib1.Examples) (parseLiteral "Examples")
 
 -- >>> parseDish "Dish \"Chicken Soup\" 250restbv"
 -- >>> parseDish "BigDish (Dish \"Chicken Soup\" 250) (Dish \"Salad\" 150)restbv"
@@ -99,30 +119,10 @@ parseDish :: Parser Lib1.Dish
 parseDish = parseSimple `orElse` parseBig
   where
     parseSimple :: Parser Lib1.Dish
-    parseSimple s =
-      case parseLiteral "Dish " s of
-        Left e -> Left e
-        Right (_, s1) -> case parseDishName s1 of
-          Left e -> Left e
-          Right (name, s2) -> case parseLiteral " " s2 of
-            Left e -> Left e
-            Right (_, s3) -> case parseInteger s3 of
-              Left e -> Left e
-              Right (cal, s4) -> Right (Lib1.Dish name cal, s4)
+    parseSimple = pmap (\(_, name, _, cal) -> Lib1.Dish name cal) (and4 (parseLiteral "Dish ") parseDishName (parseChar ' ') parseInteger)
 
     parseBig :: Parser Lib1.Dish
-    parseBig s =
-      case parseLiteral "BigDish (" s of
-        Left e -> Left e
-        Right (_, s1) -> case parseDish s1 of
-          Left e -> Left e
-          Right (d1, s2) -> case parseLiteral ") (" s2 of
-            Left e -> Left e
-            Right (_, s3) -> case parseDish s3 of
-              Left e -> Left e
-              Right (d2, s4) -> case parseLiteral ")" s4 of
-                Left e -> Left e
-                Right (_, s5) -> Right (Lib1.BigDish d1 d2, s5)
+    parseBig = pmap (\(_, d1, _, d2, _) -> Lib1.BigDish d1 d2) (and5 (parseLiteral "BigDish (") parseDish (parseLiteral ") (") parseDish (parseLiteral ")"))
 
 -- >>> parseCommand "Dump Examplesrestbv"
 -- Right (Dump Examples,"restbv")
@@ -131,33 +131,20 @@ parseDish = parseSimple `orElse` parseBig
 parseCommand :: Parser Lib1.Command
 parseCommand = pDump `orElse` pCreate `orElse` pRemove `orElse` pEat
   where
-    pDump input =
-      case and2 (parseLiteral "Dump ") parseDumpable input of
-        Right ((_, d), rest) -> Right (Lib1.Dump d, rest)
-        Left e -> Left e
+    pDump :: Parser Lib1.Command
+    pDump = pmap (\(_, dumpable) -> Lib1.Dump dumpable) (and2 (parseLiteral "Dump ") parseDumpable)
 
-    pCreate input =
-      case parseLiteral "CreateDish (" input of
-        Left e -> Left e
-        Right (_, s1) -> case parseDish s1 of
-          Left e -> Left e
-          Right (d, s2) -> case parseLiteral ")" s2 of
-            Left e -> Left e
-            Right (_, rest) -> Right (Lib1.CreateDish d, rest)
+    pCreate :: Parser Lib1.Command
+    pCreate = pmap (\(_, dish, _) -> Lib1.CreateDish dish)
+              (and3 (parseLiteral "CreateDish (") parseDish (parseLiteral ")"))
 
-    pRemove input =
-      case and2 (parseLiteral "RemoveDish ") parseDishName input of
-        Right ((_, name), rest) -> Right (Lib1.RemoveDish name, rest)
-        Left e -> Left e
+    pRemove :: Parser Lib1.Command
+    pRemove = pmap (\(_, name) -> Lib1.RemoveDish name)
+              (and2 (parseLiteral "RemoveDish ") parseDishName)
 
-    pEat input =
-      case parseLiteral "EatDish (" input of
-        Left e -> Left e
-        Right (_, s1) -> case parseDish s1 of
-          Left e -> Left e
-          Right (d, s2) -> case parseLiteral ")" s2 of
-            Left e -> Left e
-            Right (_, rest) -> Right (Lib1.EatDish d, rest)
+    pEat :: Parser Lib1.Command
+    pEat = pmap (\(_, dish, _) -> Lib1.EatDish dish)
+           (and3 (parseLiteral "EatDish (") parseDish (parseLiteral ")"))
 
 process :: Lib1.Command -> [String]
 process (Lib1.Dump Lib1.Examples) = "Examples:" : map toCliCommand Lib1.examples
@@ -179,10 +166,10 @@ ppName s = "\"" ++ s ++ "\""
 
 instance ToCliCommand Lib1.Command where
   toCliCommand :: Lib1.Command -> String
-  toCliCommand (Lib1.Dump d)         = "Dump " ++ ppDumpable d
-  toCliCommand (Lib1.CreateDish d)   = "CreateDish (" ++ ppDish d ++ ")"
-  toCliCommand (Lib1.RemoveDish nm)  = "RemoveDish " ++ ppName nm
-  toCliCommand (Lib1.EatDish d)      = "EatDish (" ++ ppDish d ++ ")"
+  toCliCommand (Lib1.Dump dumpable)         = "Dump " ++ ppDumpable dumpable
+  toCliCommand (Lib1.CreateDish dish)   = "CreateDish (" ++ ppDish dish ++ ")"
+  toCliCommand (Lib1.RemoveDish dishName)  = "RemoveDish " ++ ppName dishName
+  toCliCommand (Lib1.EatDish dish)      = "EatDish (" ++ ppDish dish ++ ")"
 
 instance Eq Lib1.Command where
   (==) :: Lib1.Command -> Lib1.Command -> Bool
